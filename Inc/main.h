c84f8679264f3251cbd9a9a970455c3745081b71
Łukasz Nidecki <qyonek+github@gmail.com>
2017-04-23 23:12:05 +0200

    Initial commit

---
+++ Inc/main.h
@@ -0,0 +1,111 @@
+/**
+  ******************************************************************************
+  * File Name          : main.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * Copyright (c) 2017 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+  /* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define green_led_Pin GPIO_PIN_13
+#define green_led_GPIO_Port GPIOC
+#define EN_2_A_Pin GPIO_PIN_0
+#define EN_2_A_GPIO_Port GPIOA
+#define EN_2_B_Pin GPIO_PIN_1
+#define EN_2_B_GPIO_Port GPIOA
+#define EN_2_BTN_Pin GPIO_PIN_2
+#define EN_2_BTN_GPIO_Port GPIOA
+#define LED_RI_Pin GPIO_PIN_3
+#define LED_RI_GPIO_Port GPIOA
+#define LED_LE_Pin GPIO_PIN_4
+#define LED_LE_GPIO_Port GPIOA
+#define EN_3_BTN_Pin GPIO_PIN_5
+#define EN_3_BTN_GPIO_Port GPIOA
+#define EN_3_A_Pin GPIO_PIN_6
+#define EN_3_A_GPIO_Port GPIOA
+#define EN_3_B_Pin GPIO_PIN_7
+#define EN_3_B_GPIO_Port GPIOA
+#define BTN_A_Pin GPIO_PIN_0
+#define BTN_A_GPIO_Port GPIOB
+#define BTN_B_Pin GPIO_PIN_1
+#define BTN_B_GPIO_Port GPIOB
+#define BTN_C_Pin GPIO_PIN_2
+#define BTN_C_GPIO_Port GPIOB
+#define BTN_D_Pin GPIO_PIN_10
+#define BTN_D_GPIO_Port GPIOB
+#define LED_DN_Pin GPIO_PIN_11
+#define LED_DN_GPIO_Port GPIOB
+#define BTN_UP_Pin GPIO_PIN_12
+#define BTN_UP_GPIO_Port GPIOB
+#define BTN_DN_Pin GPIO_PIN_13
+#define BTN_DN_GPIO_Port GPIOB
+#define BTN_LE_Pin GPIO_PIN_14
+#define BTN_LE_GPIO_Port GPIOB
+#define BTN_RI_Pin GPIO_PIN_15
+#define BTN_RI_GPIO_Port GPIOB
+#define EN_1_A_Pin GPIO_PIN_8
+#define EN_1_A_GPIO_Port GPIOA
+#define EN_1_B_Pin GPIO_PIN_9
+#define EN_1_B_GPIO_Port GPIOA
+#define EN_1_BTN_Pin GPIO_PIN_10
+#define EN_1_BTN_GPIO_Port GPIOA
+#define LED_UP_Pin GPIO_PIN_5
+#define LED_UP_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+#endif /* __MAIN_H */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
